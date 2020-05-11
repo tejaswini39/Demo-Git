8ce37e70fff820ed04c351b1202c609417b464c9
tejaswini39 <tejaswini368@gmail.com>
2020-05-11 19:45:43 +0530

    This is my first git commit of java poroject

---
+++ src/com/company/Student.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Student {
+
+        int id;
+        String name;
+
+        public Student(int i,String n)
+        {
+            id=i;
+            name=n;
+        }
+
+        public void disp()
+
+        {
+            System.out.println("Name:"+name);
+            System.out.println("ID:"+id);
+        }
+        public static void main(String[] args)
+        {
+
+            Student obj=new Student(1,"sri");
+            obj.disp();
+
+        }
+
+    }
+
+
